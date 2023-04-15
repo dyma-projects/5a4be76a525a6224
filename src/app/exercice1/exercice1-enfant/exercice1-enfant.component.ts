0d5d733d843ceeee495e5fac0b0eeb69af1cd380
rhennino <renaud.hennino@gmail.com>
2023-04-15 23:30:43 +0200

    projet3

--- src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
+++ src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-exercice1-enfant',
   templateUrl: './exercice1-enfant.component.html',
-  styleUrls: ['./exercice1-enfant.component.css']
+  styleUrls: ['./exercice1-enfant.component.css'],
 })
 export class Exercice1EnfantComponent implements OnInit {
+  @Input() compteur: number;
+  @Output() misAJourCompteur: EventEmitter<number> = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  addCompteur() {
+    this.compteur++;
+    this.misAJourCompteur.emit(this.compteur);
   }
 
+  decreaseCompteur() {
+    this.compteur--;
+    this.misAJourCompteur.emit(this.compteur);
+  }
 }
